f6667923538f1b9d86800b5a6a8cc0dd1e72530f
Pedro Matos <pedroheunapolis@gmail.com>
2024-06-08 23:44:46 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import livrosRoutes from "./livrosRoutes.js";
+import autoresRoutes from "./autoresRoutes.js";
+
+const routes = (app) => {
+    app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
+
+    app.use(express.json(), livrosRoutes);
+};
+
+export default routes;
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import LivroController from "../controllers/livroController.js";
+
+const livrosRoutes = express.Router();
+
+livrosRoutes.get("/livros", LivroController.listarLivros);
+livrosRoutes.get("/livros/:id", LivroController.listarLivroPorId);
+livrosRoutes.post("/livros", LivroController.cadastrarLivro);
+livrosRoutes.put("/livros/:id", LivroController.atualizarLivro);
+livrosRoutes.delete("/livros/:id", LivroController.deletarLivro);
+
+export default livrosRoutes;
